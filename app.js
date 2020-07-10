dc7a026053102faf1e1f760d444a0af4c054a4e2
pikselan <faif@pikselan.com>
2020-07-10 20:05:16 +0700

    add model and passport

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose"); // add mongoose
+const bodyParser = require("body-parser"); // add body parser
+const passport = require("passport"); // add passport
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersApiRouter = require("./routes/api/users"); // add users api
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// ----------------- start setup
 
-app.use(logger('dev'));
+// add body parser middleware to extract all item
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+// config mongo
+const db = require("./config/keys").mongoUrl;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("mongo connected"))
+  .catch((err) => console.log(err));
+
+// config passport
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+// ----------------- end setup
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("v1/api/users", usersApiRouter); // setup users api
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
